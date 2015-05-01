27f14feeb9c278ff65677d50e9d5ac82e7478ffd
Bryan Tsang <bvtsang@gmail.com>
2015-05-01 16:25:25 -0700

    interferes -> interfere

--- src/kosuzu/template.clj
+++ src/kosuzu/template.clj
@@ -2,7 +2,7 @@
   (:require selmer.parser))
 
 (defn fill-template [data]
-  ; The double curly braces interferes with selmer.parser,
+  ; The double curly braces interfere with selmer.parser,
   ; so we're prepending it here
   (str "{{MusicArticle\n"
        (selmer.parser/render-file "template.txt" data)))
