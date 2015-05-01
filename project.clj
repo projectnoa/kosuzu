d9a25f46cae5d6a0bc056dac6d67bde318021614
Bryan Tsang <bvtsang@gmail.com>
2015-04-30 22:25:54 -0700

    Add project.clj added via lein new.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject kosuzu "0.1.0-SNAPSHOT"
+  :description "Given a URL to a Touhou doujin album, this tool will generate a Touhou wiki article for it."
+  :url "https://github.com/bvtsang/kosuzu"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot kosuzu.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
