49ac0f0b57e81d7d365b6867290f1715de87463f
Bryan Tsang <bvtsang@gmail.com>
2015-05-01 22:27:30 -0700

    eastnewsound: Add catalogno.

--- src/kosuzu/parser/eastnewsound.clj
+++ src/kosuzu/parser/eastnewsound.clj
@@ -4,6 +4,9 @@
 (def ^:private url-pattern
   (re-pattern "/discography/ens(\\d{4})\\.html"))
 
+(defn- get-catalogno [url]
+  (str "ENS-" (last (re-find url-pattern (.getPath url)))))
+
 (defrecord EastNewSoundParser [html url]
   kosuzu.parser/Parser
   (can-parse-album?
@@ -14,6 +17,7 @@
   (parse-album
     [this]
     {:group "[[EastNewSound]]"
-     :groupCat "EastNewSound"}))
+     :groupCat "EastNewSound"
+     :catalogno (get-catalogno url)}))
 
 (defn get-parser [html url] (EastNewSoundParser. html url))
