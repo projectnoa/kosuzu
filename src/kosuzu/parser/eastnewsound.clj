8b125c31b4e8372d597c1ab28a3409dbf7d906f7
Bryan Tsang <bvtsang@gmail.com>
2015-05-01 22:54:30 -0700

    eastnewsound: Add titleen.

--- src/kosuzu/parser/eastnewsound.clj
+++ src/kosuzu/parser/eastnewsound.clj
@@ -1,8 +1,15 @@
 (ns kosuzu.parser.eastnewsound
-  (:require kosuzu.parser))
+  (:require kosuzu.parser
+            [net.cgrand.enlive-html :as html]))
 
 (def ^:private url-pattern
   (re-pattern "/discography/ens(\\d{4})\\.html"))
+
+(defn- get-titleen [html]
+  (let [titleen-pattern (re-pattern "ENS-\\d{4} / (.*)::EastNewSound Official Site::")
+        title (-> (html/select html [:title])
+                  first :content first)]
+    (last (re-find titleen-pattern title))))
 
 (defn- get-catalogno [url]
   (str "ENS-" (last (re-find url-pattern (.getPath url)))))
@@ -16,7 +23,8 @@
 
   (parse-album
     [this]
-    {:group "[[EastNewSound]]"
+    {:titleen (get-titleen html)
+     :group "[[EastNewSound]]"
      :groupCat "EastNewSound"
      :catalogno (get-catalogno url)}))
 
