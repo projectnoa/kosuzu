88becda276afc832f200a3a8245c874577774a2e
Bryan Tsang <bvtsang@gmail.com>
2015-05-01 22:21:02 -0700

    eastnewsound: Add can-parse-album? logic, and basic parse-album logic.

--- src/kosuzu/parser/eastnewsound.clj
+++ src/kosuzu/parser/eastnewsound.clj
@@ -1,10 +1,19 @@
 (ns kosuzu.parser.eastnewsound
   (:require kosuzu.parser))
 
-; http://e-ns.net/discography/ens0036.html
+(def ^:private url-pattern
+  (re-pattern "/discography/ens(\\d{4})\\.html"))
+
 (defrecord EastNewSoundParser [html url]
   kosuzu.parser/Parser
-  (can-parse-album? [this] false)
-  (parse-album [this] {}))
+  (can-parse-album?
+    [this]
+    (and (= (.getHost url) "e-ns.net")
+         (re-find url-pattern (.getPath url))))
+
+  (parse-album
+    [this]
+    {:group "[[EastNewSound]]"
+     :groupCat "EastNewSound"}))
 
 (defn get-parser [html url] (EastNewSoundParser. html url))
